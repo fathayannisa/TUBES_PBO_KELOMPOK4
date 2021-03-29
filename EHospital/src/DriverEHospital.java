bab920c4a516692979e5c839bcd551a2a488c02a
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-03-29 22:21:09 +0700

    Add files via upload

---
+++ EHospital/src/DriverEHospital.java
@@ -0,0 +1,9 @@
+import Entitas.Pasien;
+
+public class DriverEHospital {
+    public static void main(String[] args) {
+        Pasien psn = new Pasien();
+        psn.inputPasien();
+        psn.displayinfo();
+    }
+}
