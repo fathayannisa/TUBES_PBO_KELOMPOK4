a68c959f46859b5a6200d6f2a4117b794844f353
ranaaa220 <55308507+ranaaa220@users.noreply.github.com>
2021-03-29 21:40:34 +0800

    Update User.java

--- EHospital/src/Entitas/User.java
+++ EHospital/src/Entitas/User.java
@@ -10,5 +10,40 @@ package Entitas;
  * @author acer
  */
 public class User {
+    private String nama;
+    private String email;
+    private int password;
+    private String status;
     
+    public void setnama(String Nama){     
+        nama = Nama;
+    }    
+    
+    public void setEmail(String Email){
+        email = Email;
+    }
+    
+    public void setPassword(int Password){
+        password = Password;
+    }
+    
+    public void setStatus(String Status){
+        status = Status;
+    }
+    
+    public String getNama(){
+        return nama;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public int getPass(){
+        return password;
+    }
+    
+    public String getStatus(){
+        return status;
+    }
 }
