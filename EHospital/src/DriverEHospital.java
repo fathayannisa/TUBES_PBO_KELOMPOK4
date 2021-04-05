fe64ef6c23c7e06617e52d4d74853dcd9767f17c
ranaaa220 <55308507+ranaaa220@users.noreply.github.com>
2021-04-05 21:42:59 +0800

    update

--- EHospital/src/DriverEHospital.java
+++ EHospital/src/DriverEHospital.java
@@ -1,8 +1,14 @@
 import Entitas.Pasien;
+import Entitas.Dokter;
 
 public class DriverEHospital {
     public static void main(String[] args) {
         Pasien psn = new Pasien();
+        Dokter[] tabeldokter = new Dokter[10];
+        
+        tabeldokter[0] = new Dokter("dr. Fathaya", "fathaya@gmail.com", 123, "Online", "FNA","Spesialis Dokter Jantung");        
+        tabeldokter[0].dataPreview(tabeldokter); 
+        
         psn.inputPasien();
         psn.displayinfo();
     }
