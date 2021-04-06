b8f8c2eccaec6bbc7170cb3a548b3a1705226f76
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-04-06 23:16:57 +0700

    Update DriverEHospital.java

--- EHospital/src/DriverEHospital.java
+++ EHospital/src/DriverEHospital.java
@@ -1,15 +1,20 @@
-import Entitas.Pasien;
 import Entitas.Dokter;
+import Entitas.Pasien;
+import Relasi.PesanDokter;
 
 public class DriverEHospital {
     public static void main(String[] args) {
-        Pasien psn = new Pasien();
-        Dokter[] tabeldokter = new Dokter[10];
+        Pasien[] psn = new Pasien[10];
+        Dokter[] dokter = new Dokter[10];
+        PesanDokter[] tabelpesan = new PesanDokter[10]; 
+        psn[0] = new Pasien();
+        dokter[0] = new Dokter();
+        tabelpesan[0] = new PesanDokter();
         
-        tabeldokter[0] = new Dokter("dr. Fathaya", "fathaya@gmail.com", 123, "Online", "FNA","Spesialis Dokter Jantung");        
-        tabeldokter[0].dataPreview(tabeldokter); 
-        
-        psn.inputPasien();
-        psn.displayinfo();
+        dokter[0].dataPreview(dokter);
+        psn[0].dataPreview(psn);
+        tabelpesan[0].setPesanDokter(psn, 1, dokter);
+        tabelpesan[0].displayinfo(tabelpesan);
     }
 }
+
