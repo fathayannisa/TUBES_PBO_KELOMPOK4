66065bd672d6f081ef8097f019446c32cd004f0b
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-04-06 23:13:16 +0700

    Update Pasien.java

--- EHospital/src/Entitas/Pasien.java
+++ EHospital/src/Entitas/Pasien.java
@@ -1,45 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entitas;
 import java.util.Scanner;
 
-public class Pasien extends User {
+public class Pasien extends User{
     private int idpasien;
     private String lokasi;
     private int umur;
     private int tanggalLhr;
-    private String keluhan;
-    private String konsultasi;
-    private String nama;
-    private String email;
-    private int password;
     
     public void inputPasien() {
+        String nama, email;
         Scanner sc = new Scanner(System.in); 
-        System.out.println("Nama pasien : ");
+        System.out.print("Nama pasien : ");
         nama = sc.nextLine();
         super.setnama(nama);
         
-        System.out.println("Id pasien : ");
+        System.out.print("Id pasien : ");
         idpasien = sc.nextInt();
         
-        System.out.println("Lokasi : ");
+        System.out.print("Lokasi : ");
+        lokasi = sc.nextLine();
         lokasi = sc.nextLine();
         
-        System.out.println("Tanggal Lahir : ");
+        System.out.print("Tanggal Lahir : ");
         tanggalLhr = sc.nextInt();   
         
-        System.out.println("umur : ");
+        System.out.print("umur : ");
         umur = sc.nextInt();  
         
-        System.out.println("Email : ");
-        email = sc.nextLine(); 
+        System.out.print("Email : ");
+        email = sc.nextLine();
+        email = sc.nextLine();
         super.setEmail(email);
        
     }
+    
+    public void setIdPasien(int idpasien) {
+        this.idpasien = idpasien;
+    }
+    
+    public void setLokasi(String lokasi) {
+        this.lokasi = lokasi;
+    }
+    
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+    
+    public void setTanggalLahir(int tanggalLhr) {
+        this.tanggalLhr = tanggalLhr;
+    }
+     
     public int getIdPasien() {
         return idpasien;
     }
@@ -56,21 +67,42 @@ public class Pasien extends User {
         return tanggalLhr;
     }
     
-    public String getKeluhan() {
-        return keluhan;
+    public void setManualPasien(String nama, String email,int idpasien, int umur, String lokasi, int tanggalLhr){
+        super.setnama(nama);
+        super.setEmail(email);
+        this.idpasien = idpasien;
+        this.umur = umur;
+        this.lokasi = lokasi;
+        this.tanggalLhr = tanggalLhr;
     }
     
-    public String getKonsultasi() {
-        return konsultasi;
+    public void dataPreview(Pasien[] tabelPasien){
+        for (int i = 1 ; i < 2 ; i ++) {
+            tabelPasien[i] = new Pasien();
+        }
+        tabelPasien[0].setManualPasien("naga", "naga@gmail.com", 101, 12, "Bandung",21);
+        tabelPasien[1].setManualPasien("inul", "inul@gmail.com", 102, 30, "Bali", 18);
+        
+        /*for (int i=0; i<tabelPasien[0].getValue(tabelPasien); i++ ){
+            tabelPasien[i].displayinfo();
+        }*/
+    }
+    
+    public int getValue(Pasien[] total){
+        int banyak = 0;
+        while (total[banyak] != null && banyak <= total.length) {
+            banyak = banyak + 1;
+        }
+
+        return banyak;
     }
     
     public void displayinfo(){
-        System.out.println("ID Pasien" + getIdPasien());
-        System.out.println("Nama Pasien" + super.getNama());
-        System.out.println("Umur" + getUmur());
-        System.out.println("Lokasi" + getLokasi());
-        System.out.println("Tanggal Lahir" + getTanggalLahir());
-        System.out.println("Keluhan" + getKeluhan());
-        System.out.println("Konsultasi" + getKonsultasi());
+        System.out.println("ID Pasien: " + getIdPasien());
+        System.out.println("Nama Pasien: " + super.getNama());
+        System.out.println("Umur: " + getUmur());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Lokasi: " + getLokasi());
+        System.out.println("Tanggal Lahir: " + getTanggalLahir());
     }
 }
