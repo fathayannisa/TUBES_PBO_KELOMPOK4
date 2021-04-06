112b0da2d272b2ebb354c34e520a4af5d61f67cf
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-04-06 23:16:02 +0700

    Update PesanDokter.java

--- EHospital/src/Relasi/PesanDokter.java
+++ EHospital/src/Relasi/PesanDokter.java
@@ -2,9 +2,10 @@
 package Relasi;
 import Entitas.Pasien;
 import Entitas.Dokter;
+import Entitas.User;
 import java.util.Scanner;
 
-public class PesanDokter {
+public class PesanDokter extends Dokter{
     private String ruang;
     private String hariPraktek;
     private int tanggalPraktek;
@@ -12,11 +13,45 @@ public class PesanDokter {
     private int jamPraktek;
     private Pasien pasien;
     private Dokter dokter;
+    private String namaPesanDokter;
     
-    public void setPesanDokter(Pasien[] pasien, Dokter[] dokter){
+    User pengguna = new Pasien();
+    Pasien us = (Pasien) pengguna;
+    public void setPesanDokter(Pasien[] pasien, int indeks, Dokter[] dokter){
         Scanner sc = new Scanner(System.in); 
+        super.setnama(pasien[indeks].getNama());
+        System.out.print("Nama           : ");
+        System.out.println(super.getNama());
         
-        this.pasien = pasien;
+        us.setUmur(pasien[indeks].getUmur());
+        System.out.print("Umur          : ");
+        System.out.println(us.getUmur());
+        
+        us.setTanggalLahir(pasien[indeks].getTanggalLahir());
+        System.out.print("Tanggal Lahir         : ");
+        System.out.println(us.getTanggalLahir());
+        
+        us.setLokasi(pasien[indeks].getLokasi());
+        System.out.print("Lokasi         : ");
+        System.out.println(us.getLokasi());
+        
+        System.out.print("Dokter yang Tersedia ");
+        for (int i = 0; i<dokter[0].getValue(dokter); i++) {
+            System.out.println((i+1) + ".Nama Dokter : " + dokter[i].getNama());
+            System.out.println("***********************");
+        }
+        
+        System.out.print("Masukkan nama dokter : ");
+        String input = sc.nextLine();
+        
+        for (int i = 0; i<dokter[0].getValue(dokter); i++) {
+            if (input.intern() == dokter[i].getNama()) {
+                namaPesanDokter = dokter[i].getNama();
+            }
+        }
+        
+        System.out.println("Nama dokter yang dipesan: " + namaPesanDokter);
+        
         System.out.print("Ruang : ");
         ruang = sc.nextLine();
         
@@ -31,5 +66,51 @@ public class PesanDokter {
         
         System.out.print("Waktu : ");
         jamPraktek = sc.nextInt();
+    }
+    
+    public String getRuang() {
+        return ruang;
+    }
+    
+    public String getHariPraktek() {
+        return hariPraktek;
+    }
+    
+    public int getTanggalPraktek() {
+        return tanggalPraktek;
+    }
+    
+    public int getTahun() {
+        return tahun;
+    }
+    
+    public int getJamPraktek() {
+        return jamPraktek;
+    }
+    
+    public String getNamaPesanDokter() {
+        return namaPesanDokter;
+    }
+    
+    public int getValue(PesanDokter[] total){
+        int banyak = 0;
+        while (total[banyak] != null && banyak <= total.length) {
+            banyak = banyak + 1;
+        }
+
+        return banyak;
+    }
+    
+    public void displayinfo(PesanDokter[] Pesan){
+        for (int i = 0; i<Pesan[0].getValue(Pesan); i++) {
+            System.out.println("Dokter yang dipilih  : " + Pesan[i].getNamaPesanDokter());
+            System.out.println("Nama Pasien   : " + Pesan[i].getNama());
+            System.out.println("Ruang  : " + Pesan[i].getRuang());
+            System.out.println("Hari   : " + Pesan[i].getHariPraktek());
+            System.out.println("Tanggal  :" + Pesan[i].getTanggalPraktek());
+            System.out.println("Tahun  :" + Pesan[i].getTahun());
+        
+            System.out.println(" ");
+        }
     }
 }
