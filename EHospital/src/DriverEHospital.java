95e678fa708284455e27c531b75ad38fa8ee995d
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-04-08 02:10:47 +0700

    Update DriverEHospital.java

--- EHospital/src/DriverEHospital.java
+++ EHospital/src/DriverEHospital.java
@@ -8,6 +8,7 @@ public class DriverEHospital {
         Pasien[] psn = new Pasien[10];
         Dokter[] dokter = new Dokter[10];
         PesanDokter[] tabelpesan = new PesanDokter[10]; 
+        Dokter[] tabeldokter = new Dokter[10];
         int nomorAntrian = 0;
         psn[0] = new Pasien();
         dokter[0] = new Dokter();
@@ -23,21 +24,24 @@ public class DriverEHospital {
         System.out.println("Selamat Datang di E-Hospital");
         System.out.println("1. LOGIN");
         System.out.println("2. Registrasi");
-        System.out.println("Masukan Pilihan Anda : ");
+        System.out.print("Masukan Pilihan Anda :");
+        System.out.print(" ");
+        
         int Masukan = input.nextInt();
         while (Masukan != 0) {
             if (Masukan == 1){
-                System.out.println("Masukan email");
+                System.out.println(psn[0].getValue(psn));
+                System.out.print("Masukan email : ");
                 String Username = input.nextLine();
-                System.out.println("Masukan Password");
-                int Password = input.nextInt();
+                Username = input.nextLine();
                 
-                for (int i = 0; i < psn[0].getValue(psn) ; i++) {
-                    if (psn[i].getEmail().intern() == Username.intern() && psn[i].getPass() == Password){
-                        indekscari = i;
-                        Kebenaran = true;
-                    }
-                }
+                System.out.print("Masukan Password : ");
+                String Password = input.nextLine();
+                
+                Kebenaran = false;
+                indekscari = psn[0].cariDataUser(psn, Username, Password, indekscari);
+                Kebenaran = psn[0].cekKebenaran(psn, Username, Password, Kebenaran);
+                
                 
                 if (Kebenaran == true) {
                     System.out.println("Selamat Datang");
@@ -46,13 +50,18 @@ public class DriverEHospital {
                     Masukan = input.nextInt();
                     while (Masukan != 0) {
                         if (Masukan == 1) {
-                            
+                            globalindeks = tabelpesan[0].getValue(tabelpesan);
+                            tabelpesan[globalindeks] = new PesanDokter();
+                            //System.out.println(globalindeks);
+                            tabelpesan[globalindeks].setPesanDokter(psn, globalindeks+1, dokter, nomorAntrian);
+                            tabelpesan[0].displayinfo(tabelpesan);
                         } else if (Masukan == 2) {
                             
                         }
                         System.out.println("Selamat Datang");
                         System.out.println("1. Jadwal Dokter");
                         System.out.println("2. Periksa");
+                        System.out.println(" ");
                         System.out.print("Masukan Pilihan menu : ");
                         Masukan = input.nextInt();
                     }
@@ -61,18 +70,19 @@ public class DriverEHospital {
                 }
                  
             }else if (Masukan == 2) {
-                System.out.println("Registrasi Akun");
+                System.out.println(" ");
+                System.out.println("===Registrasi Akun===");
                 globalindeks = psn[0].getValue(psn);
                 psn[globalindeks] = new Pasien();
                 psn[globalindeks].inputPasien();
                 System.out.println("---");
-                psn[globalindeks].displayinfo();
+                psn[globalindeks].displayinfo(psn);
                 
             }
         System.out.println("Selamat Datang di E-Hospital");
         System.out.println("1. LOGIN");
         System.out.println("2. Registrasi");
-        System.out.println("Masukan Pilihan Anda : ");
+        System.out.print("Masukan Pilihan Anda : ");
         Masukan = input.nextInt();
         }
     }
