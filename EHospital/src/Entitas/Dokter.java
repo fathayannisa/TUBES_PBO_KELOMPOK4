10925078c2aea9e42b585dec2734d1e745a5da77
fathayannisa <55243159+fathayannisa@users.noreply.github.com>
2021-04-08 02:12:42 +0700

    Update Dokter.java

--- EHospital/src/Entitas/Dokter.java
+++ EHospital/src/Entitas/Dokter.java
@@ -4,7 +4,7 @@ public class Dokter extends User{
     private String spesialisDokter;
     private String idDokter;
     
-    public void setManualDokter(String nama, String email, int password, String status, String idDokter, String spesialisDokter){
+    public void setManualDokter(String nama, String email, String password, String status, String idDokter, String spesialisDokter){
         super.setnama(nama);
         super.setEmail(email);
         super.setPassword(password);
@@ -25,11 +25,11 @@ public class Dokter extends User{
         for (int i = 1 ; i < 5 ; i ++) {
             tabeldokter[i] = new Dokter();
         }
-        tabeldokter[0].setManualDokter("dr. Fathaya", "fathaya@gmail.com", 123, "Online", "FNA","Spesialis Dokter Jantung");
-        tabeldokter[1].setManualDokter("dr. Eral", "eral@gmail.com", 345, "Offline", "MUH","Spesialis Dokter Cinta");
-        tabeldokter[2].setManualDokter("dr. Carolin", "carol@gmail.com", 567, "Online", "CRL","Spesialis Dokter Mata");        
-        tabeldokter[3].setManualDokter("dr. Cici", "cici@gmail.com", 890, "Busy", "CCI","Spesialis Dokter Otopsi");        
-        tabeldokter[4].setManualDokter("dr. Ranran", "rana@gmail.com", 555, "Online", "RNA","Spesialis Dokter Anak");        
+        tabeldokter[0].setManualDokter("dr.Fathaya", "fathaya@gmail.com", "123", "Online", "FNA","Spesialis Dokter Jantung");
+        tabeldokter[1].setManualDokter("dr.Eral", "eral@gmail.com", "345", "Offline", "MUH","Spesialis Dokter Cinta");
+        tabeldokter[2].setManualDokter("dr.Carolin", "carol@gmail.com", "567", "Online", "CRL","Spesialis Dokter Mata");        
+        tabeldokter[3].setManualDokter("dr.Cici", "cici@gmail.com", "890", "Busy", "CCI","Spesialis Dokter Otopsi");        
+        tabeldokter[4].setManualDokter("dr.Ranran", "rana@gmail.com", "555", "Online", "RNA","Spesialis Dokter Anak");        
         
         /*for (int i=0; i<tabeldokter[0].getValue(tabeldokter); i++ ){
             tabeldokter[i].displayinfo();
@@ -46,10 +46,10 @@ public class Dokter extends User{
     }
     
     public void displayinfo(){
-        System.out.println("ID Dokter     : " + getidDokter());
-        System.out.println("Nama Dokter   : " + super.getNama());
-        System.out.println("Email Dokter  : " + super.getEmail());
-        System.out.println("Status        : " + super.getStatus());
+        System.out.println("ID Dokter         : " + getidDokter());
+        System.out.println("Nama Dokter       : " + super.getNama());
+        System.out.println("Email Dokter      : " + super.getEmail());
+        System.out.println("Status            : " + super.getStatus());
         System.out.println("Spesialis Dokter  :" + getspesialisDokter());
         System.out.println(" ");
     }
